410bc68a466c88c4018a012685bd07ae5b2202dd
Joseph Phillips <joe@manalabs.eu>
2017-11-29 10:21:07 +0100

    Adds 0 as another Xmonad workspace.

--- xmonad.hs
+++ xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
 import System.IO
+import qualified XMonad.StackSet as W
 import XMonad.Actions.CycleWS
 
 main = do
@@ -11,6 +12,7 @@ main = do
 
     xmonad $ defaultConfig
         { modMask = mod4Mask
+        , workspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
         , manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         -- Keep Xmobar visible.
@@ -32,4 +34,6 @@ main = do
         --, ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
         , ("M-<Tab>", moveTo Next NonEmptyWS)
         , ("M-S-<Tab>", moveTo Prev NonEmptyWS)
+        , ("M-0", windows $ W.greedyView "0")
+        , ("M-S-0", windows $ W.shift "0")
         ]
